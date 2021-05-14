2d82a5a816e2f3b2ef11b49022f5e12928ddd158
diegoruiz06 <diegoruiz0196@gmail.com>
2021-05-14 07:50:28 -0500

    adding chat

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket-io-clent';
+
+const Chat = () => {
+    return (
+        <h1>Chat</h1>
+    )
+}
+
+export default Chat;
