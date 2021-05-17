3c201efd4750bd7e9305578d6935f0dfca9b22fe
diegoruiz06 <diegoruiz0196@gmail.com>
2021-05-17 17:38:57 -0500

    swtiching to chat api

---
+++ chat-app/src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100"
+            projectID="0456ba6c-f2e9-4d19-8c17-0001989e6f8d"
+            userName="Diego"
+            userSecret="123123"
+        />
+    );
+}
+
+export default App;
